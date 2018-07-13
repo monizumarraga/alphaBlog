032b2626556d1fc683abab7d599514e953360e22
EC2 Default User <ec2-user@ip-172-31-40-69.us-east-2.compute.internal>
2018-07-13 07:52:51 +0000

    articles delete, list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,12 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @articles=Article.all.order("created_at DESC")
   end
 
   def new
+  	@boton="Create"
     @article = Article.new
   end
 
@@ -17,19 +21,32 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article=Article.find(params[:id])
   end
   
   def edit
+  	@boton="Update"
   end
   
   def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
   
     private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
   
   def article_params
     params.require(:article).permit(:title, :description)
