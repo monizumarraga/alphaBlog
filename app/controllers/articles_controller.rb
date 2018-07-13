b47b2ae5ffdb9aca78e3a4536941d574e28d97a4
EC2 Default User <ec2-user@ip-172-31-40-69.us-east-2.compute.internal>
2018-07-13 06:08:27 +0000

    creado articulo con vistas de nuevo y mostrar un articulo

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,37 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was successfully creted"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @article=Article.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+
+  def destroy
+  end
+  
+    private
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
