6b064df050d8b9b7ca3d0558ebf26da76fc7ded3
monizumarraga <monizumarraga.hotmail.com>
2018-07-26 11:18:59 +0000

    before_save :upload_to_s3

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,6 +2,7 @@ class Image < ApplicationRecord
     belongs_to :user
     mount_uploader :picture, PictureUploader
     validate :picture_size
+    before_save :upload_to_s3
 
     private
     
@@ -9,5 +10,11 @@ class Image < ApplicationRecord
         if picture.size > 5.megabytes
             errors.add(:picture, "should be less than 5MB")
         end
+    end
+    
+    def upload_to_s3
+        s3 = Aws::S3::Resource.new(region:'us-west-2')
+        obj = s3.bucket('alphablogimages').object('key')
+        obj.upload_file('/path/to/source/file')
     end
 end
