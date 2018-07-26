4b670030e2da12ca55fe94369373cd221afcd76a
monizumarraga <monizumarraga.hotmail.com>
2018-07-26 14:52:47 +0000

    undo

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,6 @@ class Image < ApplicationRecord
     belongs_to :user
     mount_uploader :picture, PictureUploader
     validate :picture_size
-    before_save :upload_to_s3
 
     private
     
@@ -12,9 +11,4 @@ class Image < ApplicationRecord
         end
     end
     
-    def upload_to_s3
-        s3 = Aws::S3::Resource.new(region:'AWS_REGION')
-        obj = s3.bucket('S3_BUCKET').object('AWS_SECRET_ACCESS_KEY')
-        obj.upload_file('/path/to/source/file')
-    end
 end
