a5efb11b86e4f8d82fadeac4b6a129c075fdcfef
monizumarraga <monizumarraga.hotmail.com>
2018-07-30 16:52:30 +0000

    change config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["S3_ACCESS_KEY"],                        # required unless using use_iam_profile
     aws_secret_access_key: ENV["S3_SECRET_KEY"],                        # required unless using use_iam_profile
     #use_iam_profile:       true,                         # optional, defaults to false
-    #region:                ENV["S3_REGION"],                  # optional, defaults to 'us-east-1'
+    region:                ENV["S3_REGION"],                  # optional, defaults to 'us-east-1'
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
