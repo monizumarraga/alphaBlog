ffd2c93a9ca2fb12f0b8181b914a98fe7b6489fd
EC2 Default User <ec2-user@ip-172-31-26-83.eu-west-1.compute.internal>
2018-08-04 05:32:08 +0000

    sendgrid

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message 
     include ActiveModel::Model
-    attr_accessor :name, :email, :body, :to
-    validates :name, :email, :body, :to, presence: true
+    attr_accessor :name, :email, :body, :to, :from
+    validates :name, :email, :body, :to, :from, presence: true
 end
